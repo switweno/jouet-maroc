15b9c0892d9aaf333cbf3a139c6436822fc228d9
khalid <102528868+switweno@users.noreply.github.com>
2025-03-16 17:36:15 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -2342,4 +2342,17 @@ document.getElementById("whatsappSend1").addEventListener("click", function () {
 
 
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener("click", function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute("href").substring(1);
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop,
+                behavior: "smooth"
+            });
+        }
+    });
+});
 
